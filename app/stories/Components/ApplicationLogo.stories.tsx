3984ca714853f7f9679fdda2dab7fb32e21dea2d
rajahwu <vmradford@gmail.com>
2024-04-24 06:52:29 -0400

    chore: write storybook story for ApplicationLogo component

---
+++ app/stories/Components/ApplicationLogo.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObject  } from '@storybook/react';
+
+import  ApplicationLogo  from '../../resources/js/Components/ApplicationLogo';
+
+const meta: Meta = {
+    component: ApplicationLogo ,  
+}
+
+export default meta;
+type Story = StoryObj;
+
+export const Basic: Story = { }
