5e83591b074c446353d809fe18238861076487cb
rajahwu <vmradford@gmail.com>
2024-04-25 15:28:53 -0400

    chore: add layout and pages to stories

---
+++ app/stories/Pages/Dashboard.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObject } from '@storybook/react';
+
+import Dashboard from '../../resources/js/Pages/Dashboard';
+
+const meta: Meta = {
+    component: Dashboard,
+};
+
+export default meta;
+type Story = StoryObject;
+
+export const Basic: Story = { }
