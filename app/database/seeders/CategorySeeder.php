a8e1e63531c370135b4f202642059f9a9ac56f18
rajahwu <vmradford@gmail.com>
2024-05-02 18:16:27 -0400

    chore: create ProblemSeeder

--- app/database/seeders/CategorySeeder.php
+++ app/database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,28 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create([
+            'title' => 'core'
+        ]);
+
+        Category::create([
+            'title' => 'arithmetic'
+        ]);
+
+        Category::create([
+            'title' => 'addition'
+        ]);
+
+        Category::create([
+            'title' => 'subtraction'
+        ]);
+
+        Category::create([
+            'title' => 'multiplication'
+        ]);
+        
+        Category::create([
+            'title' => 'division'
+        ]);
     }
 }
--- app/database/seeders/ProblemSeeder.php
+++ app/database/seeders/ProblemSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 use App\Models\Problem;
+use App\Models\Category;
+use App\Models\ProblemCategory;
 use DateTime;
 
 class ProblemSeeder extends Seeder
@@ -15,18 +17,77 @@ class ProblemSeeder extends Seeder
      */
     public function run(): void
     {
-          // Addition problems
-          for ($i = 1; $i <= 9; $i++) {
+        // Create or retrieve the "core" category
+        $coreCategory = Category::firstOrCreate(['title' => 'core']);
+
+        // Create or retrieve the "arithmetic" category
+        $arithmeticCategory = Category::firstOrCreate(['title' => 'arithmetic']);
+
+        // Create categories for addition, subtraction, multiplication, and division
+        $additionCategory = Category::firstOrCreate(['title' => 'addition']);
+        $subtractionCategory = Category::firstOrCreate(['title' => 'subtraction']);
+        $multiplicationCategory = Category::firstOrCreate(['title' => 'multiplication']);
+        $divisionCategory = Category::firstOrCreate(['title' => 'division']);
+
+        // Addition problems
+        for ($i = 1; $i <= 9; $i++) {
             for ($j = 1; $j <= 9; $j++) {
                 $solution = $i + $j;
                 $text = "$i + $j";
 
-                Problem::create([
+                // Create the problem
+                $problem = Problem::create([
                     'id' => Str::uuid(),
                     'text' => $text,
                     'solution' => $solution,
                     'created_at' => new DateTime(),
                 ]);
+
+                // Assign the problem to categories
+                ProblemCategory::create([
+                    'problem_id' => $problem->id,
+                    'category_id' => $coreCategory->id,
+                ]);
+                ProblemCategory::create([
+                    'problem_id' => $problem->id,
+                    'category_id' => $arithmeticCategory->id,
+                ]);
+                ProblemCategory::create([
+                    'problem_id' => $problem->id,
+                    'category_id' => $additionCategory->id,
+                ]);
+            }
+        }
+
+        // Subtraction problems
+        for ($i = 1; $i <= 9; $i++) {
+            for ($j = 1; $j <= 9; $j++) {
+                if (($i - $j) > 0) {
+                    $solution = $i - $j;
+                    $text = "$i - $j";
+
+                    // Create the problem
+                    $problem = Problem::create([
+                        'id' => Str::uuid(),
+                        'text' => $text,
+                        'solution' => $solution,
+                        'created_at' => new DateTime(),
+                    ]);
+
+                    // Assign the problem to categories
+                    ProblemCategory::create([
+                        'problem_id' => $problem->id,
+                        'category_id' => $coreCategory->id,
+                    ]);
+                    ProblemCategory::create([
+                        'problem_id' => $problem->id,
+                        'category_id' => $arithmeticCategory->id,
+                    ]);
+                    ProblemCategory::create([
+                        'problem_id' => $problem->id,
+                        'category_id' => $subtractionCategory->id,
+                    ]);
+                }
             }
         }
 
@@ -36,29 +97,27 @@ class ProblemSeeder extends Seeder
                 $solution = $i * $j;
                 $text = "$i * $j";
 
-                Problem::create([
+                // Create the problem
+                $problem = Problem::create([
                     'id' => Str::uuid(),
                     'text' => $text,
                     'solution' => $solution,
                     'created_at' => new DateTime(),
                 ]);
-            }
-        }
-        
-        // Subtraction problems
-        for ($i = 1; $i <= 9; $i++) {
-            for ($j = 1; $j <= 9; $j++) {
-                if (($i - $j) > 0) {
-                    $solution = $i - $j;
-                    $text = "$i - $j";
-                    
-                    Problem::create([
-                        'id' => Str::uuid(),
-                        'text' => $text,
-                        'solution' => $solution,
-                        'created_at' => new DateTime(),
-                    ]);
-                }
+
+                // Assign the problem to categories
+                ProblemCategory::create([
+                    'problem_id' => $problem->id,
+                    'category_id' => $coreCategory->id,
+                ]);
+                ProblemCategory::create([
+                    'problem_id' => $problem->id,
+                    'category_id' => $arithmeticCategory->id,
+                ]);
+                ProblemCategory::create([
+                    'problem_id' => $problem->id,
+                    'category_id' => $multiplicationCategory->id,
+                ]);
             }
         }
 
@@ -68,15 +127,31 @@ class ProblemSeeder extends Seeder
                 if ($i > $j && $i % $j == 0) {
                     $solution = $i / $j;
                     $text = "$i / $j";
-                    
-                    Problem::create([
+
+                    // Create the problem
+                    $problem = Problem::create([
                         'id' => Str::uuid(),
                         'text' => $text,
                         'solution' => $solution,
                         'created_at' => new DateTime(),
                     ]);
+
+                    // Assign the problem to categories
+                    ProblemCategory::create([
+                        'problem_id' => $problem->id,
+                        'category_id' => $coreCategory->id,
+                    ]);
+                    ProblemCategory::create([
+                        'problem_id' => $problem->id,
+                        'category_id' => $arithmeticCategory->id,
+                    ]);
+                    ProblemCategory::create([
+                        'problem_id' => $problem->id,
+                        'category_id' => $divisionCategory->id,
+                    ]);
                 }
             }
         }
+   
     }
 }
